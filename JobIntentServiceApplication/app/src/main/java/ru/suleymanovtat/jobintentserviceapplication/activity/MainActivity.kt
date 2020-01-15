a4b9d376e557f3caed0acb9fbc1ffee650d5fd07
suleymanovtat <suleymanovtat@gmail.com>
2020-01-15 23:11:20 +0300

    JobService

--- JobIntentServiceApplication/app/src/main/java/ru/suleymanovtat/jobintentserviceapplication/activity/MainActivity.kt
+++ JobIntentServiceApplication/app/src/main/java/ru/suleymanovtat/jobintentserviceapplication/activity/MainActivity.kt
@@ -1,12 +1,19 @@
 package ru.suleymanovtat.jobintentserviceapplication.activity
 
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import ru.suleymanovtat.jobintentserviceapplication.R
+import ru.suleymanovtat.jobintentserviceapplication.service.JobSchedulerService
 import ru.suleymanovtat.jobintentserviceapplication.service.SimpleIntentService
 import ru.suleymanovtat.jobintentserviceapplication.service.SimpleJobIntentService
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,6 +32,27 @@ class MainActivity : AppCompatActivity() {
             mIntent.putExtra("maxCountValue", 1000)
             mIntent.putExtra("label", "JobIntentService")
             SimpleJobIntentService().enqueueWork(this, mIntent)
+        }
+        btnJobSchedulerService.setOnClickListener {
+            val serviceName = ComponentName(this, JobSchedulerService::class.java)
+
+            val jobInfo = JobInfo.Builder(344, serviceName)
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
+                .setRequiresDeviceIdle(true)
+                .setRequiresCharging(true)
+                .setPeriodic(10_000)
+                .setPersisted(true)
+                .build()
+
+
+            val scheduler: JobScheduler =
+                getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
+            val result = scheduler.schedule(jobInfo)
+            if (result == JobScheduler.RESULT_SUCCESS) {
+                Log.e("my", "RESULT_SUCCESS")
+            } else {
+                Log.e("my", "RESULT_FAILURE")
+            }
         }
     }
 }
